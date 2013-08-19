c19863843ac8e30ac4dfc077ade0c31bc35edea1
Evan Salazar <esalazar1981@gmail.com>
2013-08-18 22:27:50 -0600

    Made some changed to models

--- electroInv/models.py
+++ electroInv/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from chucho.models import ChuchoManager, ChuchoUserManager
 
 class Manufacture(models.Model):
@@ -6,39 +7,76 @@ class Manufacture(models.Model):
     website = models.CharField(max_length=100,blank=True,null=True)
     contact = models.CharField(max_length=100,blank=True,null=True)
     objects = ChuchoManager()
+    def can_view(self, user):
+        if not isinstance(user, get_user_model()):
+            raise TypeError('%s is not an auth user' % str(user))
+
+        return True
+    def __unicode__(self):
+        return self.name
+
 
 class Vendor(models.Model):
     name = models.CharField(max_length=100)
     website = models.CharField(max_length=100,blank=True,null=True)
     contact = models.CharField(max_length=100,blank=True,null=True)
     objects = ChuchoManager()
+    def can_view(self, user):
+        if not isinstance(user, get_user_model()):
+            raise TypeError('%s is not an auth user' % str(user))
+
+        return True
+    def __unicode__(self):
+        return self.name
+
 
 class Type(models.Model):
     name = models.CharField(max_length=100)
     reference = models.CharField(max_length=100)
     objects = ChuchoManager()
+    def can_view(self, user):
+        if not isinstance(user, get_user_model()):
+            raise TypeError('%s is not an auth user' % str(user))
+
+        return True
+    def __unicode__(self):
+        return self.name
 
 class Package(models.Model):
     name = models.CharField(max_length=100)
     library = models.CharField(max_length=100,blank=True,null=True)
     objects = ChuchoManager()
+    def can_view(self, user):
+        if not isinstance(user, get_user_model()):
+            raise TypeError('%s is not an auth user' % str(user))
+
+        return True
+    def __unicode__(self):
+        return self.name
 
 
 class Part(models.Model):
-    manufactureNumber = models.CharField(max_length=100,blank=True,null=True)
+    part_number = models.CharField(max_length=100,blank=True,null=True)
     description = models.CharField(max_length=100,blank=True,null=True)
     value = models.CharField(max_length=100,blank=True,null=True)
     manufacture = models.ForeignKey('Manufacture')
-    partType = models.ForeignKey('Type')
+    part_type = models.ForeignKey('Type')
     package = models.ForeignKey('Package')
     location = models.CharField(max_length=100,blank=True,null=True)
     vendor = models.ForeignKey('Vendor')
-    vendorNumber = models.CharField(max_length=100,blank=True,null=True)
+    vendor_number = models.CharField(max_length=100,blank=True,null=True)
     qty = models.IntegerField(default=0) #Set default to 0
     price = models.FloatField(default=0)
     created = models.DateTimeField(auto_now_add=True)
-    lastUpdate = models.DateTimeField(auto_now=True)
+    last_update = models.DateTimeField(auto_now=True)
     objects = ChuchoManager()
+    def can_view(self, user):
+        if not isinstance(user, get_user_model()):
+            raise TypeError('%s is not an auth user' % str(user))
+
+        return True
+    def __unicode__(self):
+        return self.part_number
 
 class Log(models.Model):
     timestamp = models.DateTimeField(auto_now=True)
@@ -50,4 +88,11 @@ class Log(models.Model):
     invoice = models.CharField(max_length=100,blank=True,null=True)
     price = models.FloatField()
     objects = ChuchoManager()
+    def can_view(self, user):
+        if not isinstance(user, get_user_model()):
+            raise TypeError('%s is not an auth user' % str(user))
+
+        return True
+
+
 
