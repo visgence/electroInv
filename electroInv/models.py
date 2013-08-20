33235789cece6ef85afe65739d007aa18f75f06a
Evan Salazar <esalazar1981@gmail.com>
2013-08-20 12:51:52 -0600

    Auto add to log on save or modify

--- electroInv/models.py
+++ electroInv/models.py
@@ -59,11 +59,11 @@ class Part(models.Model):
     part_number = models.CharField(max_length=100,blank=True,null=True)
     description = models.CharField(max_length=100,blank=True,null=True)
     value = models.CharField(max_length=100,blank=True,null=True)
-    manufacture = models.ForeignKey('Manufacture')
-    part_type = models.ForeignKey('Type')
-    package = models.ForeignKey('Package')
+    manufacture = models.ForeignKey('Manufacture',blank=True,null=True)
+    part_type = models.ForeignKey('Type',blank=True,null=True)
+    package = models.ForeignKey('Package',blank=True,null=True)
     location = models.CharField(max_length=100,blank=True,null=True)
-    vendor = models.ForeignKey('Vendor')
+    vendor = models.ForeignKey('Vendor',blank=True,null=True)
     vendor_number = models.CharField(max_length=100,blank=True,null=True)
     qty = models.IntegerField(default=0) #Set default to 0
     price = models.FloatField(default=0)
@@ -78,15 +78,26 @@ class Part(models.Model):
     def __unicode__(self):
         return self.part_number
 
+    def save(self, *args, **kwargs):
+
+        action = "Added New Part"
+        
+        if self.id is not None:
+            action = "Modified Part"
+
+        super(Part, self).save(*args, **kwargs)
+        l = Log(part=self,action=action)
+        l.save()
+
 class Log(models.Model):
     timestamp = models.DateTimeField(auto_now=True)
     part = models.ForeignKey('Part')
     action = models.CharField(max_length=100,blank=True,null=True)
-    qty = models.IntegerField() #Negative for removing
+    qty = models.IntegerField(blank=True,null=True) #Negative for removing
     note = models.CharField(max_length=100,blank=True,null=True)
     vendor = models.CharField(max_length=100,blank=True,null=True)
     invoice = models.CharField(max_length=100,blank=True,null=True)
-    price = models.FloatField()
+    price = models.FloatField(blank=True,null=True)
     objects = ChuchoManager()
     def can_view(self, user):
         if not isinstance(user, get_user_model()):
