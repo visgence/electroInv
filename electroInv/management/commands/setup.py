43dadd2e07e1c9b5aa1553ebd77904dc8af3e57e
djsubaku@gmail.com <bmurphy@visgence.com>
2013-08-22 19:07:57 -0600

    Add setup command

---
+++ electroInv/management/commands/setup.py
@@ -0,0 +1,26 @@
+
+#system imports
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        call_command('syncdb', interactive=False)
+        # self.stdout.write('Successfully BLAHHHED' )#DEBUG
+                 
+        #Comment or uncomment fixtures as needed for loading.
+        #ORDER OF FIXTURES MATTERS!! Some have dependencies on others.
+        print "Loading test data fixtures"
+
+        fixtures = [
+            [
+                'electroInv/fixtures/user.json'
+            ]
+        ]
+
+        #Load fixtures
+        for apps in fixtures:
+            for fixture in apps:
+                call_command('loaddata', fixture, verbosity=1)
+
