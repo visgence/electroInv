fd02b0a3b1bc26fd1cdcd1af8d7ca79bfac7691a
esalazar <esalazar@esalazar-MacBookAir.(none)>
2013-08-26 14:48:30 -0600

    Started csv import tools

---
+++ electroInv/utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#Tools for parsing 
+
+#Read in csv file and return a list represenging the row data
+def parseDigikeyCSV(strdata):   
+    
+    data = []
+    return data
+
+
+#Read in cmp file and return a list representing the data
+def parseKiCadCMP(strdata):
+    data = []
+    return data	
+
+
+
