c679b3b9b8964dc44460deccc946e75402b67d33
Michael Harris <shiftycow@gmail.com>
2015-03-03 16:22:50 -0700

    updates wsgi.py to the usual format

--- electroInv/wsgi.py
+++ electroInv/wsgi.py
@@ -1,32 +1,19 @@
-"""
-WSGI config for partsInv project.
+#!/usr/bin/python
 
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
-
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
-"""
 import os
+import re
+import sys 
 
-# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
-# if running multiple sites in the same mod_wsgi process. To fix this, use
-# mod_wsgi daemon mode with each site in its own daemon process, or use
-# os.environ["DJANGO_SETTINGS_MODULE"] = "partsInv.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "partsInv.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "electroInv.settings")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
+# in order to run more than one instance on an Apache server, we
+# need to figure out our path dynamically
+program_path = os.path.realpath(__file__)
+APP_PATH = re.sub('django.wsgi$','',program_path);
+sys.path.append(APP_PATH)
+
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
