3604c1d14df31bf719982550ba9511cb4d1337eb
Cyrille Gindreau <cyrille@visgence.com>
2016-10-29 15:44:02 -0600

    conformed url patterns to django 1.10, patterns are no longer used. Updated chucho urls as well

--- electroInv/urls.py
+++ electroInv/urls.py
@@ -1,22 +1,21 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+import views
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
 
-urlpatterns = patterns(
-    'electroInv.views',
-    url(r'^$', 'index', name='index'),
-    url(r'^login/$', 'login', name='login'),
-    url(r'^logout/$', 'logout', name='logout'),
+    url(r'^digikey/$', views.digikey, name='digikey'),
+    url(r'^digikey/import/$', views.importDigikey, name='import-digikey'),
 
-    url(r'^digikey/$', 'digikey', name='digikey'),
-    url(r'^digikey/import/$', 'importDigikey', name='import-digikey'),
+    url(r'^octopart/$', views.octopart, name='octopart'),
+    url(r'^octopart/update/$', views.octopartUpdate, name='octopart-update'),
 
-    url(r'^octopart/$', 'octopart', name='octopart'),
-    url(r'^octopart/update/$', 'octopartUpdate', name='octopart-update'),
-
-    url(r'^part/$', 'part', name='part'),
-    url(r'^vendor/$', 'vendor', name="vendor"),
-    url(r'^type/$', 'type', name="type"),
-    url(r'^manufacture/$', 'manufacture', name='manufacture'),
-    url(r'^package/$', 'package', name='package'),
-    url(r'^log/$', 'log', name="log"),
-    url(r'^login-page/$', 'login_page', name='login-page'),
-)
+    url(r'^part/$', views.part, name='part'),
+    url(r'^vendor/$', views.vendor, name="vendor"),
+    url(r'^type/$', views.type, name="type"),
+    url(r'^manufacture/$', views.manufacture, name='manufacture'),
+    url(r'^package/$', views.package, name='package'),
+    url(r'^log/$', views.log, name="log"),
+    url(r'^login-page/$', views.login_page, name='login-page'),
+]
